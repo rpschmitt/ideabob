f3937d6ab00fdada2c5d533183b0d0380462bd0c
Roberto Schmitt <52758257+rpschmitt@users.noreply.github.com>
2021-11-08 12:14:04 -0300

    up
    up

---
+++ c3/prot-v2/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1636384381,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/solidground.png",
+		"images/player-sheet0.png",
+		"images/camera-sheet0.png",
+		"images/button-sheet0.png",
+		"images/spritefont.png",
+		"images/tiledbg.png",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
